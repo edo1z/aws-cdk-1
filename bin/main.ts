f450be3859f764a20d2bf7b7af98a8e34fa62170
web3ten0 <web3ten0@gmail.com>
2023-02-10 23:49:20 +0900

    add ApiGateway and Lambda function

--- bin/main.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import "source-map-support/register";
-import * as cdk from "aws-cdk-lib";
-import { HogeStack } from "../lib/hoge-stack";
-
-const app = new cdk.App();
-new HogeStack(app, "HogeStack");
--- lib/hoge-stack.ts
+++ lib/hoge-stack.ts
@@ -1,12 +1,35 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import { LambdaRestApi, LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
 export class HogeStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    new s3.Bucket(this, "MyFirstHogeBucket", {
+
+    // s3
+    const bucket = new s3.Bucket(this, "hoge_bucket", {
       versioned: true,
     });
+
+    // Lambda
+    const lambdaFn = new NodejsFunction(this, "hoge_lambda", {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      memorySize: 1000,
+      timeout: cdk.Duration.seconds(30),
+    });
+    if (lambdaFn.role) {
+      bucket.grantReadWrite(lambdaFn.role);
+    }
+
+    // Api gateway
+    const api = new LambdaRestApi(this, "hoge_apigateway", {
+      handler: lambdaFn,
+      proxy: false,
+    });
+    const hoge = api.root.addResource("hoge");
+    hoge.addMethod("GET", new LambdaIntegration(lambdaFn));
   }
 }
